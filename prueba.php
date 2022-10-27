c7587e661ea49903ab70df4668f795d2959b808d
juan06 <juan.viva05@misena.edu.co>
2022-10-27 08:38:22 -0500

    first commit

---
+++ prueba.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suma</title>
+</head>
+<body>
+    <form action="suma.php" method="post">
+        <label for="">Num1</label><input type="text" name="num1">
+        <label for="">Num2</label><input type="text" name="num2">
+        <input type="submit" name="enviar" value="enviar">
+    </form>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php
+    //declaracion de variables
+    $numero = 5;
+    $num2 = 2;
+    $suma = $numero + $num2;
+
+    //$cadena = "yo soy una cadena de texto";
+
+    echo $numero + $num2;
+    echo "<br>";
+    echo $suma;
+    /* echo "<br>";
+    echo $cadena;
+    echo "<br>";
+    echo $numero." ".$cadena; */
+
+?>
